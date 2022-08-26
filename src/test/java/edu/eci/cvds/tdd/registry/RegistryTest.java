2cb9ade85d0b8eb22c842160653733a421dcece7
daniel.ochoa-u <danochoa1412@gmail.com>
2022-08-26 18:27:43 -0500

    Pruebas de edad

--- src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -13,5 +13,40 @@ public class RegistryTest {
         Assert.assertEquals(RegisterResult.VALID, result);
     }
 
+    @Test
+    public void validateSuperiorLimitAge(){
+        Person person = new Person("Daniel", 123456, 1001, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+
+    @Test
+    public void validateValidAge(){
+        Person person = new Person("Daniel", 123456, 20, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+
+    @Test
+    public void validateUnderageAge(){
+        Person person = new Person("Daniel", 123456, 5, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    @Test
+    public void validateLowerLimiteAge(){
+        Person person = new Person("Daniel", 123456, -5, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+
+    @Test
+    public void validateSupériorLimitName(){
+        Person person = new Person("Danielqqqqqqqqqqqqqqqqqqqqqqqqqq", 123456, 20, Gender.MALE, true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
     // TODO Complete with more test cases
 }
